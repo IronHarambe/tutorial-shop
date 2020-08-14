e3a624592cf109790ffc3f9a512334702601747c
IronHarambe <49575350+IronHarambe@users.noreply.github.com>
2020-08-14 14:25:01 +1000

    homepage init

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
 
+import Homepage from "./homepage";
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Homepage></Homepage>
+    </div>
+  );
 }
 
 export default App;
